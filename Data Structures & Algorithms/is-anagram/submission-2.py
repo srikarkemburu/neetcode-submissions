136fbab639ef4ab5c42751750cf904fcc1465ff1
srikarkemburu <srikar.kemburi@gmail.com>
2026-08-28 12:01:14 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+
+'''
+elif set(s) != set(t):
+            return False
+        else:
+            for c, k in zip(sorted(s), sorted(t)):
+                if s.count(c) != t.count(k):
+                    return False
+            return True
+'''
+
+
